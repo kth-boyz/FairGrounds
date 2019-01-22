cf957d84b9982d4e884a1bb7b96795bee5fc67e6
PettaP <peters3@kth.se>
2019-01-22 14:18:00 +0100

    First commit

---
+++ src/main/java/FairGrounds/Main.java
@@ -0,0 +1,16 @@
+package FairGrounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    /**
+     * Starts the bank application.
+     *
+     * @param args There are no command line parameters.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
---
+++ src/main/java/FairGrounds/Presentation/FairGroundsController.java
@@ -0,0 +1,19 @@
+package FairGrounds.Presentation;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Scope("session")
+public class FairGroundsController {
+
+    static final String DEFAULT_PAGE_URL = "/";
+    static final String EXAMPLE_PAGE = "testpage";
+
+    @GetMapping(DEFAULT_PAGE_URL)
+    public String showDefaultView() {
+        return EXAMPLE_PAGE;
+    }
+
+}
