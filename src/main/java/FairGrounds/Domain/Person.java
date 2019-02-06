511236576da1872f5a6178f846f8657c42b5b401
PettaP <peters3@kth.se>
2019-02-06 21:10:47 +0100

    Added authentication

--- src/main/java/FairGrounds/Domain/Person.java
+++ src/main/java/FairGrounds/Domain/Person.java
@@ -1,5 +1,7 @@
 package FairGrounds.Domain;
 
+import FairGrounds.Presentation.RegisterDTO;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
--- src/main/java/FairGrounds/Presentation/ExceptionHandler.java
+++ src/main/java/FairGrounds/Presentation/ExceptionHandler.java
@@ -12,8 +12,9 @@ public class ExceptionHandler implements ErrorController{
     private static final String ERROR_PATH = "failure";
     public static final String ERROR_PAGE_URL = "error";
     public static final String ERROR_TYPE_KEY = "errorType";
-    public static final String USER_NOT_FOUND = "user-not-found";
-    public static final String INVALID_PWD = "invalid.password";
+    public static final String USER_NOT_FOUND = "The specified user could not be found";
+    public static final String INVALID_PWD = "The password you entered was incorrect";
+    public static final String USERNAME_TAKEN = "The username is already taken";
 
     @Override
     public String getErrorPath() {
