cc6d2417a3d9e89e73a23e2e51dd60863f074ef7
Taoudi <Taoudi128@hotmail.com>
2019-02-21 00:05:05 +0100

    core functionality completed

--- src/main/java/FairGrounds/Presentation/ApplicationDetailController.java
+++ src/main/java/FairGrounds/Presentation/ApplicationDetailController.java
@@ -19,8 +19,8 @@ import java.util.Date;
 public class ApplicationDetailController {
 
     private final static String APPLICATION_URL = "/app";
-    private final static String APPLICATION_PAGE = "applicationDetail";
-
+    private final static String APPLICATION_PAGE = "admin/applicationDetail";
+    private final static String LIST_URL = "/list-application";
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat( "yyyy-MM-dd"), true));
@@ -33,7 +33,7 @@ public class ApplicationDetailController {
     @Autowired
     ApplicationDetailForm applicationDetailForm;
 
-    @RequestMapping("/"+APPLICATION_URL+"/{appid}")
+    @GetMapping("/"+APPLICATION_URL+"/{appid}")
     public String showExpertiseView(@ModelAttribute("ApplicationDetailForm") ApplicationDetailForm applicationDetailForm, Model model,@PathVariable("appid") int appid) {
         Long id = new Long(appid);
         Application app = applicationService.getApplication(id);
@@ -44,14 +44,34 @@ public class ApplicationDetailController {
         return APPLICATION_PAGE;
     }
 
-    @PostMapping(value="/"+APPLICATION_URL+"/{appid}" ,params={"type"})
-    public String acceptApplication(@ModelAttribute("ApplicationDetailForm") ApplicationDetailForm applicationDetailForm, Model model,@PathVariable("appid") int appid) {
+    @PostMapping(value="/"+APPLICATION_URL+"/{appid}" )
+    public String acceptApplication(@ModelAttribute("ApplicationDetailForm") ApplicationDetailForm applicationDetailForm, Model model
+            ,@PathVariable("appid") int appid,@RequestParam(value="type",required=false) String type) {
+        Long id = new Long(appid);
         Application application = this.applicationDetailForm.getApplication();
-        application.setStatus("ACCEPTED");
-        applicationService.storeApplication(application);
-        System.out.printf("check dbase");
-        applicationDetailForm=this.applicationDetailForm;
-        model.addAttribute(applicationDetailForm);
-        return APPLICATION_PAGE;
+
+        switch (type) {
+            case "accept":
+                 application.setStatus("ACCEPTED");
+                applicationService.storeApplication(application);
+                applicationDetailForm=this.applicationDetailForm;
+                model.addAttribute("id",id.toString());
+                model.addAttribute(applicationDetailForm);
+                return APPLICATION_PAGE;
+            case "reject":
+                application.setStatus("REJECTED");
+                applicationService.storeApplication(application);
+                applicationDetailForm=this.applicationDetailForm;
+                model.addAttribute("id",id.toString());
+                model.addAttribute(applicationDetailForm);
+                return APPLICATION_PAGE;
+            case "goback":
+                applicationDetailForm = null;
+                return "redirect:" + LIST_URL;
+
+                default:
+                    return "redirect:" +APPLICATION_URL + id.toString();
+
+        }
     }
 }
