f50aa36f3c91cf3720b22fde2be9e583d4e8996c
PettaP <peters3@kth.se>
2019-03-01 12:10:13 +0100

    JavaDocs Complete

--- src/main/java/FairGrounds/Repository/ExpertiseRepository.java
+++ src/main/java/FairGrounds/Repository/ExpertiseRepository.java
@@ -4,12 +4,26 @@ import FairGrounds.Domain.Expertise;
 import FairGrounds.Domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 @Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface ExpertiseRepository  extends JpaRepository<Expertise, Integer> {
+
+    /**
+     *Fins all Expertise
+     * @return a list of Expertise
+     */
     @Override
     List<Expertise> findAll();
 
+    /**
+     * Finds an expertise by its name
+     * @param name the name of the expertise
+     * @return an expertise
+     */
     Expertise findByName(String name);
 }
