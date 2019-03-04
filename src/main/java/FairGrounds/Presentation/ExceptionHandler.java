5ec8fcd0dbc9aa625858875e1d30b35f9765fbb7
Taoudi <taoudi128@gmail.com>
2019-03-04 14:36:13 +0100

    exception

--- src/main/java/FairGrounds/Presentation/ExceptionHandler.java
+++ src/main/java/FairGrounds/Presentation/ExceptionHandler.java
@@ -1,9 +1,15 @@
 package FairGrounds.Presentation;
 
 
+import FairGrounds.Domain.IllegalApplicationException;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static com.sun.corba.se.impl.activation.ServerMain.APPLICATION_ERROR;
 
 @Controller
 @ControllerAdvice
@@ -15,6 +21,7 @@ public class ExceptionHandler implements ErrorController{
     public static final String USER_NOT_FOUND = "The specified user could not be found";
     public static final String INVALID_PWD = "The password you entered was incorrect";
     public static final String USERNAME_TAKEN = "The username is already taken";
+    public static final String APPLICATION_ERROR = "appError";
 
     /**
      *
@@ -24,4 +31,13 @@ public class ExceptionHandler implements ErrorController{
     public String getErrorPath() {
         return "/" + ERROR_PATH;
     }
+
+    @ExceptionHandler(IllegalApplicationException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String applicationError(Exception exception, Model model) {
+        model.addAttribute(ERROR_TYPE_KEY, APPLICATION_ERROR);
+        return ERROR_PAGE_URL;
+    }
+
 }
+
