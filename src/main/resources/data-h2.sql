7626fb3bac6334c3569c6fdc125c20c2c25ddf6e
Taoudi <taoudi128@gmail.com>
2019-03-04 21:44:07 +0100

    sql

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -51,27 +51,13 @@ INSERT INTO APPLICATION (status, applicationdate, id, person) VALUES
 INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (2,1,2,2);
 INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (3,3,3,1);
 INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (4,3,4,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (5,3,5,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (6,3,6,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (7,3,7,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (8,3,8,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (9,3,9,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (10,3,10,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (11,2,10,70);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (12,3,12,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (13,3,13,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (14,3,14,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (15,3,15,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (16,3,16,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (17,3,17,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (18,3,18,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (19,3,19,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (20,3,20,1);
-INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (21,3,21,1);
+INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (5,1,5,1);
+INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (6,3,5,1);
+INSERT INTO EXPERTISEPROFILE (id, expertise,application,years) VALUES (7,2,5,1);
 
 
 
 INSERT INTO AVAILABILITY (id, fd, td, application) VALUES (1, '1999-01-02 00:00:00', '2000-02-05 00:00:00',2);
-INSERT INTO AVAILABILITY (id, fd, td, application) VALUES (2, '2002-01-02 00:00:00', '2004-02-03 00:00:00',10);
-INSERT INTO AVAILABILITY (id, fd, td, application) VALUES (3, '1999-01-02 00:00:00', '2000-02-01 00:00:00',10);
+INSERT INTO AVAILABILITY (id, fd, td, application) VALUES (2, '2002-01-02 00:00:00', '2004-02-03 00:00:00',5);
+INSERT INTO AVAILABILITY (id, fd, td, application) VALUES (3, '1999-01-02 00:00:00', '2000-02-01 00:00:00',5);
 
